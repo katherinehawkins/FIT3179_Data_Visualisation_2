7d0b1bf8af3995b350c87d6a3e3e40d4f136608e
Katherine Hawkins <khaw0004@student.monash.edu>
2024-10-13 11:29:35 +1100

    Finalised Data Visualisation

--- bubble_chart.js
+++ bubble_chart.js
@@ -2,15 +2,15 @@ var vg_1 = "combined_map_bubble.json";
 var vg_2 = "homework_w9_31561764.json";
 var vg_3 = "inflation_chart.json";
 var vg_4 = "stacked_bar.json";
-vegaEmbed("#bubble_chart", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-vegaEmbed("#map_chart", vg_2).then(function(result) {
+vegaEmbed("#bubble_chart", vg_1, {"actions": false}).then(function (result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-    }).catch(console.error);
-    vegaEmbed("#inflation_chart", vg_3).then(function(result) {
-        // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-        }).catch(console.error);
-        vegaEmbed("#bar_chart", vg_4).then(function(result) {
-            // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-            }).catch(console.error);
+}).catch(console.error);
+vegaEmbed("#map_chart", vg_2, {"actions": false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#inflation_chart", vg_3, {"actions": false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#bar_chart", vg_4, {"actions": false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
